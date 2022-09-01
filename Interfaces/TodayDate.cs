7edc3bb9cf15e99040436d4b79bbb0d69e097480
Lerato <oanyanelerato17@gmail.com>
2022-09-01 15:39:30 +0200

    Added an abstract class TodayDate to create date automatically a record is created

---
+++ Interfaces/TodayDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Device_Management_System.Interfaces
+{
+    public abstract class TodayDate
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+    }
+}
