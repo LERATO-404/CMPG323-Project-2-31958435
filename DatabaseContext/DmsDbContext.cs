53955300be49347a6820c329153969cf63b08f28
Lerato <oanyanelerato17@gmail.com>
2022-09-01 11:53:26 +0200

    Added DbContext class to represent a database connection and a set of tables Categories, Zones, and Devices

---
+++ DatabaseContext/DmsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Device_Management_System.Models;
+
+namespace Device_Management_System.DatabaseContext
+{
+    public class DmsDbContext : DbContext
+    {
+
+        public DmsDbContext(DbContextOptions<DmsDbContext> options) : base(options)
+        {
+
+        }
+
+        //create table the tables
+        public DbSet<Zone> Zones { get; set; }
+        public DbSet<Category> Categories { get; set;}
+        public DbSet<Device> Devices { get; set; }
+
+
+
+
+    }
+}
