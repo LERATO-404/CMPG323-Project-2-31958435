59fa028bea061a9a26582e62dd0e1d9a5f3fdede
Lerato <oanyanelerato17@gmail.com>
2022-09-04 21:50:26 +0200

    Added the authentication folder with all the models for user registration and login also the DbContext

---
+++ Authentication/Response.cs
@@ -0,0 +1,11 @@
+
+
+namespace Device_Management_System.Authentication
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+
+}
---
+++ Authentication/UserRoles.cs
@@ -0,0 +1,10 @@
+
+namespace Device_Management_System.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        
+    }
+}
